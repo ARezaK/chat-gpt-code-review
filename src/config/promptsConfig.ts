cd5c7dc614ff0429475d7f740609e66f0d372f79
ARezaK <ARezaK@users.noreply.github.com>
2024-04-30 00:06:14 -0400

    Update promptsConfig.ts

--- src/config/promptsConfig.ts
+++ src/config/promptsConfig.ts
@@ -4,7 +4,7 @@ enum Prompt {
 
 const promptsConfig: { [key in Prompt]: string } = {
   [Prompt.SYSTEM_PROMPT]:
-    'You now assume the role of a code reviewer. Based on the patch provide a list of suggestions how to improve the code with examples according to coding standards and best practices. Keep your suggestions short and concise.\nStart every suggestion with path to the file. Path to the file should start with @@ and end with @@',
+    'You now assume the role of a code reviewer. Based on the patch provide a list of suggestions how to improve the code with examples according to coding standards and best practices. Keep your suggestions short and concise. Do not suggest removing comments.\nStart every suggestion with path to the file. Path to the file should start with @@ and end with @@',
 };
 
 export default promptsConfig;
